4a533e0df1b19d4968d11fa004170dd8c09ed154
Matthew Harshman <mjharshman66@gmail.com>
2020-01-30 04:19:08 -0800

    implement branch table for instruct functs

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,35 +4,111 @@ import sys
 
 class CPU:
     """Main CPU class."""
+
+    global instructions
     instructions = {
-        130 : 'LDI'
+        1 : "HLT",
+        71 : "PRN",
+        130 : "LDI",
+        162 : "MUL"
     }
+
+    global branch_table
+    branch_table = {}
+
+    # global IR
+    # IR = 0
+
     def __init__(self):
         """Construct a new CPU."""
         self.ram = [0]*256
         self.register = [0]*8
-        self.pc=0
+        self.IR=0
+        
+
+    
+    def handle_hlt(self):
+        sys.exit()
+    
+    branch_table[instructions[1]] =  handle_hlt
+
+    
+    def handle_ldi(self, op1, op2):
+        self.register[op1] = op2
+        self.IR+=3
+        
+    branch_table[instructions[130]] = handle_ldi
+
+    
+    def handle_prn(self, op1):
+        val = self.register[op1]
+        print(val)
+        self.IR += 2
+        
+    branch_table[instructions[71]] = handle_prn
+
+    
+    def handle_mul(self, op1, op2):
+        val1 = self.register[op1]
+        val2 = self.register[op2]
+        val3 = val1 * val2
+        self.register[op1] = val3
+        self.IR +=3
+
+    branch_table[instructions[162]] = handle_mul
+
 
     def load(self):
         """Load a program into memory."""
+        
+        if len(sys.argv) != 2:
+            print("Usage: <file.py> filename", file=sys.stderr)
+            sys.exit(1)
+        
+        try:
+            address = 0
 
-        address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+
+                    #ignore comments
+                    split_comments = line.split("#")
+                    instruct = split_comments[0].split()
+                    # print('instruct', instruct)
+
+                    #ignore blank lines
+                    if instruct == []:
+                        continue
+
+                    #print(instruct)
+                    value = int(instruct[0],2)
+                    # print('val', value)
+                    
+
+                    self.ram[address] = value
+                    address +=1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
+
+
+
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -72,44 +148,71 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        
-        IR = self.pc
+        # global IR
+        # IR = self.pc
 
         #load 8 into register 0
         # print register 0
         #halt
+
         running = True
         while running:
 
-            command = self.ram[IR]
-            
+            command = self.ram[self.IR]
+            #print('this',command)
+            cmnd = instructions[command]
+            if cmnd == 'HLT':
+                running = False
+                continue
+            func = branch_table[cmnd]
+
             #number of instructions
             byte_string = f'{command:b}'
+            if len(byte_string) < 8:
+                byte_string = byte_string.rjust(8,'0')
+            # print(byte_string)
             slice_of_instructs = byte_string[:2]
+            # print(slice_of_instructs)
             num_of_instructs = int(slice_of_instructs,2)
+            # print(cmnd, num_of_instructs)
             
             if num_of_instructs == 2:
-                operand_a = self.ram_read(IR+1)
-                operand_b = self.ram_read(IR+2)
+                operand_a = self.ram_read(self.IR+1)
+                operand_b = self.ram_read(self.IR+2)
+                func(self, operand_a, operand_b)
+                continue
 
             if num_of_instructs == 1:
-                operand_a = self.ram_read(IR+1)
-
-            if command == 0b10000010: # load LDI
-                
-                load_reg = operand_a # register to load
-                val = operand_b # value to insert
-
-                self.register[load_reg] = val
-                IR+=3
+                operand_a = self.ram_read(self.IR+1)
+                func(self, operand_a)
                 continue
+
             
-            elif command == 0b01000111: # print code
-                reg_to_print = operand_a # register value is to be printed from
-                val_to_print = self.register[reg_to_print] # value from register
-                print(val_to_print) # 
-                IR+=2
-                continue
+            
+            # if cmnd == "LDI": # load LDI
+                
+            #     load_reg = operand_a # register to load
+            #     val = operand_b # value to insert
 
-            elif command == 0b00000001: # halt
-                running = False
+            #     self.register[load_reg] = val
+            #     IR+=3
+            #     continue
+            
+            # elif cmnd == "PRN": # print code
+            #     reg_to_print = operand_a # register value is to be printed from
+            #     val_to_print = self.register[reg_to_print] # value from register
+            #     print(val_to_print) # 
+            #     IR+=2
+            #     continue
+
+            # elif cmnd == "MUL":
+            #     val1 = self.register[operand_a]
+            #     val2 = self.register[operand_b]
+            #     val3 = val1 * val2
+            #     self.register[operand_a] = val3
+            #     IR+=3
+            #     continue
+
+
+            # elif cmnd == "HLT": # halt
+            #     sys.exit()
